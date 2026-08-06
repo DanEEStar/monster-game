e4e7870485b2166333487e9d90bb59cba83a1f05
build <build@0x86.xyz>
2026-08-06 17:23:21 +0200

    add battle screen

--- src/scenes/Battle.ts
+++ src/scenes/Battle.ts
@@ -7,56 +7,84 @@ import Phaser from "phaser";
 /* END-USER-IMPORTS */
 
 export default class Battle extends Phaser.Scene {
-  constructor() {
-    super("Battle");
 
-    /* START-USER-CTR-CODE */
+	constructor() {
+		super("Battle");
+
+		/* START-USER-CTR-CODE */
     // Write your code here.
     /* END-USER-CTR-CODE */
-  }
+	}
 
-  editorCreate(): void {
-    // background
-    const background = this.add.rectangle(640, 360, 1280, 720);
-    background.isFilled = true;
-    background.fillColor = 3868516;
+	editorCreate(): void {
 
-    // title
-    const title = this.add.text(640, 300, "", {});
-    title.setOrigin(0.5, 0.5);
-    title.text = "Battle Scene";
-    title.setStyle({
-      color: "#ffffff",
-      fontFamily: "Arial",
-      fontSize: "52px",
-      fontStyle: "bold",
-    });
+		// rival_name
+		const rival_name = this.add.text(497, 95, "", {});
+		rival_name.setOrigin(0.5, 0.5);
+		rival_name.text = "Elefant";
+		rival_name.setStyle({ "color": "#e9d5ff", "fontFamily": "Arial", "fontSize": "24px" });
 
-    // status
-    const status = this.add.text(640, 370, "", {});
-    status.setOrigin(0.5, 0.5);
-    status.text = "Ready for development";
-    status.setStyle({ color: "#e9d5ff", fontFamily: "Arial", fontSize: "24px" });
+		// backButton
+		const backButton = this.add.rectangle(128, 689, 250, 56);
+		backButton.isFilled = true;
+		backButton.fillColor = 3359061;
 
-    // backButton
-    const backButton = this.add.rectangle(640, 620, 250, 56);
-    backButton.isFilled = true;
-    backButton.fillColor = 3359061;
+		// backLabel
+		const backLabel = this.add.text(123, 692, "", {});
+		backLabel.setOrigin(0.5, 0.5);
+		backLabel.text = "Back to Menu";
+		backLabel.setStyle({ "color": "#ffffff", "fontFamily": "Arial", "fontSize": "22px" });
 
-    // backLabel
-    const backLabel = this.add.text(640, 620, "", {});
-    backLabel.setOrigin(0.5, 0.5);
-    backLabel.text = "Back to Menu";
-    backLabel.setStyle({ color: "#ffffff", fontFamily: "Arial", fontSize: "22px" });
+		// rival_image
+		const rival_image = this.add.image(1022, 227, "elefant");
+		rival_image.scaleX = 0.09425493338876229;
+		rival_image.scaleY = 0.11490248000323693;
 
-    this.backButton = backButton;
+		// player_image
+		const player_image = this.add.image(555, 362, "gorilla");
+		player_image.scaleX = 0.5395141376172515;
+		player_image.scaleY = 0.42157410449095606;
 
-    this.events.emit("scene-awake");
-  }
+		// rival_hp_total
+		const rival_hp_total = this.add.rectangle(651, 133, 128, 128);
+		rival_hp_total.scaleX = 3.0231262345729277;
+		rival_hp_total.scaleY = 0.21683839264859162;
+		rival_hp_total.isFilled = true;
 
-  private backButton!: Phaser.GameObjects.Rectangle;
+		// rival_hp_level
+		const rival_hp_level = this.add.rectangle(559, 133, 128, 128);
+		rival_hp_level.scaleX = 1.5963051320779478;
+		rival_hp_level.scaleY = 0.21071865694711023;
+		rival_hp_level.isFilled = true;
+		rival_hp_level.fillColor = 6061006;
 
-  /* START-USER-CODE */
+		// player_name
+		const player_name = this.add.text(779, 427, "", {});
+		player_name.setOrigin(0.5, 0.5);
+		player_name.text = "Gorilla";
+		player_name.setStyle({ "color": "#e9d5ff", "fontFamily": "Arial", "fontSize": "24px" });
+
+		// player_hp_total
+		const player_hp_total = this.add.rectangle(933, 465, 128, 128);
+		player_hp_total.scaleX = 3.0231262345729277;
+		player_hp_total.scaleY = 0.21683839264859162;
+		player_hp_total.isFilled = true;
+
+		// player_hp_level
+		const player_hp_level = this.add.rectangle(841, 465, 128, 128);
+		player_hp_level.scaleX = 1.5963051320779478;
+		player_hp_level.scaleY = 0.21071865694711023;
+		player_hp_level.isFilled = true;
+		player_hp_level.fillColor = 6061006;
+
+		this.backButton = backButton;
+
+		this.events.emit("scene-awake");
+	}
+
+	private backButton!: Phaser.GameObjects.Rectangle;
+
+	/* START-USER-CODE */
 
   create() {
     this.editorCreate();
