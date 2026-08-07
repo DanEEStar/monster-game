3f0a7cf36e2e27d3fe1ca2d75dca5159bca70254
Daniel Egger <daniel.egger@gmail.com>
2026-08-07 09:50:24 +0200

    reformat code

--- src/scenes/BattleController.ts
+++ src/scenes/BattleController.ts
@@ -2,47 +2,47 @@
 
 /* START OF COMPILED CODE */
 export class BattleController {
-    player = {
-        hp: 100,
-        defense: 0,
-        attack: 10,
-    };
+  player = {
+    hp: 100,
+    defense: 0,
+    attack: 10,
+  };
 
-    rival = {
-        hp: 100,
-        defense: 0,
-        attack: 10,
-    };
+  rival = {
+    hp: 100,
+    defense: 0,
+    attack: 10,
+  };
 
-    public getPlayer() {
-        return this.player;
+  public getPlayer() {
+    return this.player;
+  }
+
+  public getRival() {
+    return this.rival;
+  }
+
+  public rival_turn() {
+    switch (Math.abs(Math.random() * 2)) {
+      case 0:
+        return;
+      case 1:
+        this.rival.defense += 0.1 * Math.random();
+        return;
+      case 2:
+        this.player.hp =
+          this.player.hp -
+          this.rival.attack * Math.random() * 2 * (1 - this.player.defense);
+        return;
     }
+  }
 
-    public getRival() {
-        return this.rival;
-    }
+  public defense() {
+    this.player.defense += 0.1 * Math.random();
+  }
 
-    public rival_turn() {
-        switch (Math.abs(Math.random() * 2)) {
-            case 0:
-                return;
-            case 1:
-                this.rival.defense += 0.1 * Math.random();
-                return;
-            case 2:
-                this.player.hp =
-                    this.player.hp -
-                    this.rival.attack * Math.random() * 2 * (1 - this.player.defense);
-                return;
-        }
-    }
-
-    public defense() {
-        this.player.defense += 0.1 * Math.random();
-    }
-
-    public attack() {
-        this.rival.hp =
-            this.rival.hp - this.player.attack * Math.random() * 2 * (1 - this.rival.defense);
-    }
+  public attack() {
+    this.rival.hp =
+      this.rival.hp - this.player.attack * Math.random() * 2 * (1 - this.rival.defense);
+  }
 }
